9ff633dec32c623c24615c21a464c7468a66ed77
Ehsan <ehsan.doroudian@gmail.com>
2024-02-09 22:06:10 +0330

    symbol

--- generate_password.py
+++ generate_password.py
@@ -2,6 +2,7 @@ import random
 from string import ascii_lowercase, ascii_uppercase
 from default_setting import password_generation_options as settings
 
+
 def find_true_options(settings : dict):
     true_options = []
     for option in settings:
@@ -19,8 +20,12 @@ def add_uppercase():
     choice = random.choice(ascii_uppercase)
     return choice
 
-
-    
+def add_symbol():
+    symbol_1 = random.choice(range(33, 48))
+    symbol_2 = random.choice(range(91,97))
+    symbol_3 = random.choice(range(123, 127))
+    choice = random.choice([symbol_1, symbol_2, symbol_3])
+    return chr(choice)
 
 def generate_random_password(true_options : list):
     my_password = ""
@@ -37,8 +42,9 @@ def generate_random_password(true_options : list):
             password = add_uppercase()
             my_password += password
         
-        
-            
+        elif random_choice == "symbol":
+            password = add_symbol()
+            my_password += password
             
             
     
