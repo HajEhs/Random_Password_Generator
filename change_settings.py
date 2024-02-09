439a6d30051928b55e896d504f25061b6d82f2ab
Ehsan <ehsan.doroudian@gmail.com>
2024-02-09 21:16:07 +0330

    minor change(change file's name)

---
+++ change_settings.py
@@ -0,0 +1,52 @@
+from default_setting import password_generation_options as options
+from errors import InvalidUserInputError, LengthLimitError
+
+
+new_setting = []
+for key, value in options.items():
+    print("enter -> {}".format(value))
+    
+    if key != "length":
+        while True:
+            user_choice = input(f"Do you want to have {key}:[y/n] ")
+            print("-" * 20)
+            try:
+                if user_choice.lower() not in ['y', 'n', ""]:
+                    raise InvalidUserInputError
+                else:
+                    if user_choice == "":
+                        user_choice = value
+                    elif user_choice == "y":
+                        user_choice = True
+                    else:
+                        user_choice = False
+                    new_setting.append((key, user_choice))
+                    break       
+
+            except InvalidUserInputError:
+                print("Your input is invalid.[y/n/enter]")
+                print("-" * 20)
+    
+    else:
+        while True:
+            
+            try:
+                length = int(input("length(between 4 to 20): "))
+                print("-" * 20)
+                if length < 4 or length > 20:
+                    raise LengthLimitError
+                new_setting.append((key, length))
+                break
+                
+            except ValueError:
+                print("You must enter an integer number.")
+                print("-" * 20)
+
+            except LengthLimitError:
+                print("Your number must be between 4 to 20. ")
+                print("-" * 20)
+                 
+for item in new_setting:
+    options.update({item[0]: item[1]})            
+            
+print(options)
